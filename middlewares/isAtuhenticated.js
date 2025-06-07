7ab0fc9944f01f767b5352cae0139a3e9140293b
urvichavda18 <chavdaurvi463@gmail.com>
2025-06-07 16:57:02 +0530

    Update isAtuhenticated.js

--- middlewares/isAtuhenticated.js
+++ middlewares/isAtuhenticated.js
@@ -1,27 +1,29 @@
-import jwt from "jsonwebtoken";
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
-const isAuthenticated = async (req,res, next)=>{
-    try {
-        const token = req.cookies.token;
-        console.log(token);
-        if(!token){
-            return res.status(401).json({
-                message:"User not Atuhenticated",
-                success:false
-            })
-        }
-        const decode = await jwt.verify(token,process.env.SECRET_KEY)
-        // console.log("SECRET_KEY: ", process.env.SECRET_KEY);
-        if(!decode){
-            return res.status(401).json({
-                message:"User not Atuhenticated",
-                success:false
-            })
-        };
-        req.id= decode.userId;
-        next();
-    } catch (error) {
-        console.log(error)
+    if (!token) {
+      return res.status(401).json({
+        message: "User not Authenticated",
+        success: false,
+      });
     }
-}
-export default isAuthenticated;
+
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
+    if (!decode) {
+      return res.status(401).json({
+        message: "User not Authenticated",
+        success: false,
+      });
+    }
+
+    req.id = decode.userId;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: "Invalid token",
+      success: false,
+    });
+  }
+};
